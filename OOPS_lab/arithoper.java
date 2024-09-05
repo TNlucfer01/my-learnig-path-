596b5567882e8f703435dae27d4c66c83a42da17
Aathi <ihdaa2006@gmail.com>
2024-09-05 21:31:01 +0530

    oops lab

---
+++ OOPS_lab/arithoper.java
@@ -0,0 +1,26 @@
+public class arithoper{
+public static void main(String[] args){
+int a,b,c,d,e,ar,sr,dr,mr,modr;
+a=10;
+b=2;
+c=4;
+e=7;
+d=5;
+ar=a+b;
+sr=a-b;
+mr=c*d;
+modr=a%d;
+dr=d/c;
+
+System.out.println("=====================================");
+System.out.println("======================================");
+System.out.println("DEVELOPED BY: AATHI.S \n REGISTER NO: 511523205002 ");
+System.out.println("======================================");
+System.out.println("Hello WORLD!"); 
+System.out.println("ADDTION (A+B):"+ar);
+System.out.println("SUBRACTION (A-B)"+sr);
+System.out.println("multiplication (C*D)"+mr);
+System.out.println("DIVIDE (d/c)"+dr);
+System.out.println("MODULAUS (a%d)"+modr);
+
+}}
---
+++ OOPS_lab/helloWorld.java
@@ -0,0 +1,10 @@
+
+public class helloWorld {
+    public static void main(String [] args){
+       System.out.println("======================================");
+       System.out.println("DEVELOPED BY: AATHI.S \n REGISTER NO: 511523205002 ");
+       System.out.println("======================================");
+       System.out.println("Hello WORLD!"); 
+ }
+    
+}
